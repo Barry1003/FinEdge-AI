0f72e170f8c1f41908b500ec5ed49951e922f834
Barry1003 <barryargent1003@gmail.com>
2025-11-24 12:36:25 +0100

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Dashboard from "./Dashboard";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return <Dashboard />;
 }
